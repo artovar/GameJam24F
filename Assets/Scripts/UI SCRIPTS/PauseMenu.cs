d20a3ef486ab20336edaec5f22561c1e3d43e7fd
Dorian <aarenast15@gmail.com>
2020-02-22 20:03:01 +0100

    Pause Menu Completed

---
+++ Assets/Scripts/UI SCRIPTS/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public bool GamePause = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        
+        }
+    }
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePause = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePause = true;
+    }
+
+    public void BackToMenu() {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    
+    }
+
+    public void QuitGame() {
+
+        Debug.Log("Has salido del juego desde el menu de pausa");
+    
+    }
+
+}
