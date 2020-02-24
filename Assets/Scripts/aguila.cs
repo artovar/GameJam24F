c588496cf672904a106f83cc7f9f33721fe2a656
AndreaGold99 <andreainazuma@gmail.com>
2020-02-24 13:35:12 +0100

    Aguila que no se mueve
    No se porque

---
+++ Assets/Scripts/aguila.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aguila : MonoBehaviour
+{
+    //Variables
+    Animator anim;
+    bool girar = true;
+    SpriteRenderer aguilaRender;
+    Rigidbody2D aguilaBody;
+    public float vel = 3f;
+
+    //Funciones -----------------------------------------
+
+    private void Voltear()
+    {
+        girar = !girar;
+        aguilaRender.flipX = !aguilaRender.flipX;
+    }
+
+    //-----------------------------------------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        aguilaRender = GetComponent<SpriteRenderer>();
+        aguilaBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aguilaBody.velocity = new Vector2(Time.deltaTime * vel, aguilaBody.velocity.y);
+    }
+}
